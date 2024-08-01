192f6a973f32a214fce8e61908eb15d65459b090
ganesh-bhumarapu <ganesh.bhumarapu@gmail.com>
2024-08-01 16:50:39 +0530

    Adding new File

---
+++ SpirallyTraverssingAMatrix.java
@@ -0,0 +1,53 @@
+import java.util.*;
+class SpirallyTraverssingAMatrix{
+    public static ArrayList<Integer> spirallyTraverse(int[][] matrix){
+        ArrayList<Integer> ans = new ArrayList<>();
+        int top = 0;
+        int bottom = matrix.length - 1;
+        int left = 0;
+        int right = matrix[0].length - 1;
+
+        while (top <= bottom && left <= right) {
+            for (int j = left; j <= right; j++) {
+                ans.add(matrix[top][j]);
+            }
+            top++;
+
+            for (int i = top; i <= bottom; i++) {
+                ans.add(matrix[i][right]);
+            }
+            right--;
+
+            if (top <= bottom) {
+                for (int j = right; j >= left; j--) {
+                    ans.add(matrix[bottom][j]);
+                }
+                bottom--;
+            }
+
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    ans.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] matrix = new int[n][m];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        ArrayList<Integer> res = spirallyTraverse(matrix);
+        for(int i : res){
+            System.out.println(i);
+        }
+    }
+}
